97557975ab2f350c83d2a4cceeaa31757066c97d
Ryan <ryahgohjq@gmail.com>
2018-01-26 08:08:36 -0800

    Initial commit

---
+++ source/50_editor.sh
@@ -0,0 +1,33 @@
+# Editing
+
+export EDITOR=vim
+
+if [[ ! "$SSH_TTY" ]]; then
+  if is_osx; then
+    # [[ ! "$TMUX" ]] && EDITOR=mvim
+    EDITOR=mvim
+  else
+    EDITOR=gvim
+  fi
+  export LESSEDIT="$EDITOR ?lm+%lm -- %f"
+  export GIT_EDITOR="$EDITOR -f"
+fi
+
+export VISUAL="$EDITOR"
+
+function q() {
+  if [[ -t 0 ]]; then
+    $EDITOR "$@"
+    # pwd
+    # if [[ "$1" ]]; then
+    #   $EDITOR --servername "$PWD" --remote-silent "$@"
+    # else
+    #   $EDITOR --servername "$PWD" --remote-silent "$PWD"
+    # fi
+  else
+    # Read from STDIN (and hide the annoying "Reading from stdin..." message)
+    $EDITOR - > /dev/null
+  fi
+}
+alias qv="q $DOTFILES/link/.{,g}vimrc +'cd $DOTFILES'"
+alias qs="q $DOTFILES"
---
+++ source/50_osx.sh
@@ -0,0 +1,39 @@
+# OSX-only stuff. Abort if not OSX.
+is_osx || return 1
+
+# APPLE, Y U PUT /usr/bin B4 /usr/local/bin?!
+PATH="/usr/local/bin:$(path_remove /usr/local/bin)"
+export PATH
+
+# Trim new lines and copy to clipboard
+alias c="tr -d '\n' | pbcopy"
+
+# Make 'less' more.
+[[ "$(type -P lesspipe.sh)" ]] && eval "$(lesspipe.sh)"
+
+# Start ScreenSaver. This will lock the screen if locking is enabled.
+alias ss="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
+
+# # Export Localization.prefPane text substitution rules.
+# function txt_sub_backup() {
+#   local prefs=~/Library/Preferences/.GlobalPreferences.plist
+#   local backup=$DOTFILES/conf/osx/NSUserReplacementItems.plist
+#   /usr/libexec/PlistBuddy -x -c "Print NSUserReplacementItems" "$prefs" > "$backup" &&
+#   echo "File ~${backup#$HOME} written."
+# }
+
+# # Import Localization.prefPane text substitution rules.
+# function txt_sub_restore() {
+#   local prefs=~/Library/Preferences/.GlobalPreferences.plist
+#   local backup=$DOTFILES/conf/osx/NSUserReplacementItems.plist
+#   if [[ ! -e "$backup" ]]; then
+#     echo "Error: file ~${backup#$HOME} does not exist!"
+#     return 1
+#   fi
+#   cmds=(
+#     "Delete NSUserReplacementItems"
+#     "Add NSUserReplacementItems array"
+#     "Merge '$backup' NSUserReplacementItems"
+#   )
+#   for cmd in "${cmds[@]}"; do /usr/libexec/PlistBuddy -c "$cmd" "$prefs"; done
+# }
